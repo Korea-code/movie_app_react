a9cc67249d0d3967561c7636b3c16af8d1ed2185
Korea-code <rlawlgy93@gmail.com>
2020-02-10 00:42:16 -0500

    Updates

---
+++ precache-manifest.865db0f8f2bdb5dd25fed4274bbed6be.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00ece8b39e62bb57475f9a053f361fa0",
+    "url": "/movie_app_react/index.html"
+  },
+  {
+    "revision": "cbbad0256cd3d169a6c8",
+    "url": "/movie_app_react/static/css/main.107289e3.chunk.css"
+  },
+  {
+    "revision": "70e84318d65e22caa855",
+    "url": "/movie_app_react/static/js/2.1c932c0d.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/movie_app_react/static/js/2.1c932c0d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cbbad0256cd3d169a6c8",
+    "url": "/movie_app_react/static/js/main.63f5bdd4.chunk.js"
+  },
+  {
+    "revision": "e4ecbbbdd566b98bdf5c",
+    "url": "/movie_app_react/static/js/runtime-main.cfea8c06.js"
+  }
+]);
